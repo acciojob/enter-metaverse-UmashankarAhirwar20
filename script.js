0e07006d7dcde88842269acb567c062d28eaad2c
Umashankar Ahirwar <157135198+UmashankarAhirwar20@users.noreply.github.com>
2025-11-15 23:41:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+let status = document.querySelector("#status");
+
+document.getElementById("enterBtn").onclick = function () {
+	status.innerHTML = "<h1>Enetered Metaverse</h1>"
+}
